a65760d8e2e96bc6ae4286f658a98bbe575997a7
Marco Bomben <marco.bomben@cern.ch>
2022-06-29 13:32:54 +0200

    create-3D-map.pl: modified create-3D-map.pl to use it with 100 um thick sensors

--- create-3D-map.pl
+++ create-3D-map.pl
@@ -2,14 +2,14 @@
 
 use strict;
 
-my $basename='Ez-fei4-200um-fl0-z=';
+my $basename='map_2D_z_';
 my $basesuff='_EField.dat';
 
-my $outputFileName="EField-3D-map-fl0-100V-noedge.txt";
+my $outputFileName="EField-3D-map-rd53a-50x50-100um-fl0-50V-noedge.txt";
 
 open(OUTPUT,">$outputFileName") || die "Unable to open outputFileName file $outputFileName: $?\n";
 
-for ( my $z = 1; $z<200; $z++ ) {
+for ( my $z = 1; $z<100; $z++ ) {
   my $Slice2D = $basename.$z.$basesuff;
   print "$z\t$Slice2D\n";
   open(DATA,"$Slice2D") || die "Unable to open the Slice2D file $Slice2D: $?\n";
